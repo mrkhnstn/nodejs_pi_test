3a6aec23f1d7d745306fed053fa4f8db7b584c0c
Mark Hauenstein <mark.hauenstein@gmail.com>
2012-08-15 15:33:17 +0100

    asdf

--- app.js
+++ app.js
@@ -43,11 +43,22 @@ var gpios = [];
 // setup all available gpios
 for(var i=0; i<gpioPinIds.length; i++){
 	var id = gpioPinIds[i];
+	
+	var f = function(){
+		var a = id;
+		return function(){
+			console.log("pin " + a + " ready");
+			
+			gpios[a].on("change",function(val){
+				console.log("pin " + a + " changed to " + val);
+			});
+			console.log();
+		}
+	}
+	
 	gpios[id] = gpio.export(id, {
 		direction: 'out',	
-		ready: function(){
-			console.log("pin ready: " + id); 
-		}
+		ready: f()
 	});
 }
 
@@ -55,21 +66,54 @@ app.get('/', function(req, res){
   res.send('RPi says hello :)');
 });
 
+// get gpios
+app.get('/gpio', function(req,res){
+	var s = "";
+	for(var i=0; i<gpios.length; i++){
+		var g = gpios[i];
+		if(g)
+			s += g.headerNum + " | " +g.direction + " | " + g.value + "<br>"; 
+	}
+	res.send(s);
+});
+
 // set gpio pin to input
 app.get('/gpio/in/:pin', function(req,res){
-	res.send('ok');
+	var pin = Number(req.params.pin);
+	if(gpioPinIds.indexOf(pin) == -1){
+		res.send('Error: pin' + req.params.pin + ' does not exist.');
+		return;
+	} else {
+		gpios[pin].setDirection('in');
+		res.send('OK');
+	}
 });
 
 // set gpio pin to output
 app.get('/gpio/out/:pin', function(req,res){
-	res.send('ok');
+var pin = Number(req.params.pin);
+	if(gpioPinIds.indexOf(pin) == -1){
+		res.send('Error: pin' + req.params.pin + ' does not exist.');
+		return;
+	} else {
+		gpios[pin].setDirection('out');
+		res.send('OK');
+	}
 });
 
-// get value of gpio pint
+// get value of gpio pin
 app.get('/gpio/:pin', function(req, res){
-  res.send('return value for pin ' + req.params.pin);
+  //res.send('return value for pin ' + req.params.pin);
+	var pin = Number(req.params.pin);
+	if(gpioPinIds.indexOf(pin) == -1){
+		res.send('Error: pin' + req.params.pin + ' does not exist.');
+		return;
+	} else {
+		res.send(gpios[pin].value+'');
+	}
 });
 
+// set value of gpio pin
 var booleanStrings = ["0","1"];
 app.get('/gpio/:pin/:value', function(req, res){
 	var pin = Number(req.params.pin);
