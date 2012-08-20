87f474f7e2ba111fa75945c8b12e8663a2184dbe
Mark Hauenstein <mark.hauenstein@gmail.com>
2012-08-20 11:06:22 +0100

    ws tests

---
+++ ws_client_test.js
@@ -0,0 +1,8 @@
+var WebSocket = require('ws')
+  , ws = new WebSocket('ws://127.0.0.1:8080');
+ws.on('open', function() {
+    ws.send('something');
+});
+ws.on('message', function(message) {
+    console.log('received: %s', message);
+});
