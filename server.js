a49ca1046e70be0a24d2f0f6925c37b0557be85a
Mark Hauenstein <mark.hauenstein@gmail.com>
2012-09-18 18:07:23 +0100

    webbynode redis test

--- server.js
+++ server.js
@@ -16,6 +16,12 @@ var redisSocketServer;
 // start server
 ////////////////////////////////////////////////////////////////////////////
 
+var redis = require("redis");
+var redisClient = redis.createClient();
+redisClient.on('ready',function(){
+	console.log('redisClient connected');
+});
+
 var app = express();
 
 app.configure(function(){
